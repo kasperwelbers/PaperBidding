b41072d687bc3c5ca75c7e90b1e73b580af94ad9
Kasper Welbers <kasperwelbers@gmail.com>
2023-11-02 10:47:21 +0100

    fix for stupid safelink

---
+++ src/app/auth/email/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useSearchParams } from 'next/navigation';
+
+export default function EmailSignin() {
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get('token') || '';
+  const callbackUrl = searchParams.get('callbackUrl') || '';
+  const email = searchParams.get('email') || '';
+
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="w-full bg-secondary p-8 border-b-2 border-primary">
+        <h1 className="text-5xl font-bold mb-2 text-center">Paper Bidding</h1>
+        <h4 className="text-center mt-4 mb-0">
+          Paper bidding website of the ICA Computational Methods Division
+        </h4>
+      </div>
+      <div className="mt-20">
+        <form action="/api/auth/callback/email" method="get">
+          <h1>Sign in with your email</h1>
+          {/* remove `type` and `value` if you want the user to type this manually */}
+          <input type="hidden" name="token" value={token} />
+          <input type="hidden" name="callbackUrl" value={callbackUrl} />
+          <input type="hidden" name="email" value={email} />
+          <Button type="submit" className="w-full">
+            Complete sign in
+          </Button>
+        </form>
+      </div>
+    </main>
+  );
+}
--- src/auth/authOptions.ts
+++ src/auth/authOptions.ts
@@ -2,28 +2,29 @@ import { NextAuthOptions } from 'next-auth';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import db from '@/drizzle/schema';
 import { canCreateProject } from '@/lib/authenticate';
+import EmailProvider from 'next-auth/providers/email';
+import crypto from 'node:crypto';
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 
   adapter: DrizzleAdapter(db),
+
   providers: [
-    {
-      id: 'email',
-      type: 'email',
-      from: 'ICA-Computational-Methods@middlecat.net',
-      server: {},
-      maxAge: 24 * 60 * 60,
-      name: 'Email',
-      options: {},
+    EmailProvider({
       async sendVerificationRequest(params) {
-        const { identifier, url, provider } = params;
+        const { identifier, provider, token, theme } = params;
+        const url = new URL(params.url);
+        // url.searchParams.delete("token") // uncomment if you want the user to type this manually
+        const signInURL = new URL(`/auth/email?${url.searchParams}`, url.origin);
+        const escapedHost = signInURL.host.replace(/\./g, '&#8203;.');
 
         const email = {
-          from: provider.from,
           to: identifier,
-          subject: 'Sign in to CM Paperbidding',
-          html: `Please <a href="${url}">Click here</a> to sign in`
+          from: { email: 'cm@middlecat.net', name: 'ICA Computational Methods' },
+          subject: `Sign in to CM Paper Bidding`,
+          text: `Sign in on CM Paper Bidding using the verification code: ${token}`,
+          html: `<body style="background: #f9f9f9;"><table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: #fff; max-width: 600px; margin: auto; border-radius: 10px;"> <tr> <td align="center" style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: #444;"> Sign in to <strong>${escapedHost}</strong></td></tr><tr> <td align="center" style="padding: 20px 0;"> <table border="0" cellspacing="0" cellpadding="0"> <tr> <td align="center" style="border-radius: 5px;" bgcolor="${theme.brandColor}"><a href="${signInURL}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${theme.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${theme.brandColor}; display: inline-block; font-weight: bold;">Sign in</a></td></tr></table> </td></tr><tr> <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: #444;"> If you did not request this email you can safely ignore it. </td></tr></table></body>`
         };
 
         const response = await fetch(process.env.MIDDLECAT_MAIL || '', {
@@ -35,14 +36,12 @@ export const authOptions: NextAuthOptions = {
           method: 'POST'
         });
 
-        console.log(response.ok);
-
         if (!response.ok) {
           const { errors } = await response.json();
           throw new Error(JSON.stringify(errors));
         }
       }
-    }
+    })
   ],
   callbacks: {
     async session({ session, user }) {
