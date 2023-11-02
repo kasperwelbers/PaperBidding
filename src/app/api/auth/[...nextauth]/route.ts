a1e68224489cf38688bfb899a7ae11c78a2bc06c
Kasper Welbers <kasperwelbers@gmail.com>
2023-11-02 09:25:48 +0100

    intercept safelink requests

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,20 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/auth/authOptions';
+import { NextResponse } from 'next/server';
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export async function GET(...args: any[]) {
+  const req = args[0];
+  if (req.method === 'HEAD') {
+    // intercept HEAD requests to prevent safelink checks from invalidating email sign-in links
+    // https://next-auth.js.org/tutorials/avoid-corporate-link-checking-email-provider#disable-safelink
+    return NextResponse.json({}, { status: 200 });
+  }
+
+  return handler(...args);
+}
+
+export async function POST(...args: any[]) {
+  return handler(...args);
+}
